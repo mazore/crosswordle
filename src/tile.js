ef40609d760f1401e07b789afcfdf4a19d4be590
mazore <49827495+mazore@users.noreply.github.com>
2022-02-11 13:07:20 -0800

    Fix

--- src/tile.js
+++ src/tile.js
@@ -3,12 +3,12 @@ import ps from './parameters.js';
 export default function Tile(gameGrid, gridX, gridY) {
     this.gridX = gridX; 
     this.gridY = gridY;
-    this.left = SQUARE_WIDTH*gridX + SQUARE_PADDING/2;
-    this.top = SQUARE_WIDTH*gridY + SQUARE_PADDING/2;
-    this.drawWidth = SQUARE_WIDTH - SQUARE_PADDING;
+    this.left = ps.SQUARE_WIDTH*gridX + ps.SQUARE_PADDING/2;
+    this.top = ps.SQUARE_WIDTH*gridY + ps.SQUARE_PADDING/2;
+    this.drawWidth = ps.SQUARE_WIDTH - ps.SQUARE_PADDING;
     
     this.draw = () => {
-        rect(this.ctx, this.left, this.top, this.drawWidth, this.drawWidth, BLANK_TILE_BG, true);
+        rect(this.ctx, this.left, this.top, this.drawWidth, this.drawWidth, ps.BLANK_TILE_BG, true);
     }
     this.draw();
 }
