5b3a288a225878c1d111ff082e33c992a4a4bf20
mazore <49827495+mazore@users.noreply.github.com>
2022-02-11 13:05:23 -0800

    Fix

--- src/parameters.js
+++ src/parameters.js
@@ -1,7 +1,7 @@
 function Parameters() {
     this.GAME_GRID_WIDTH = 400;
     this.GRID_WIDTH = 8;
-    this.SQUARE_WIDTH = GAME_GRID_WIDTH / GRID_WIDTH;
+    this.SQUARE_WIDTH = this.GAME_GRID_WIDTH / this.GRID_WIDTH;
     this.SQUARE_PADDING = 6; // Should be even probably
     this.BLANK_TILE_BG = '#dfe6e9';
 }
